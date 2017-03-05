0f9661cbb5d9c1641a5c709a03d80dc4dbe7dc6a
jim <jim.dev@qq.com>
2017-03-05 20:28:36 +0800

    opt

--- init_project.sh
+++ init_project.sh
@@ -8,20 +8,10 @@ mkdir -p log tigger amount http cookies captcha/archives
 touch user.list
 chmod 777 log tigger amount http cookies captcha/archives captcha user.list
 echo "qwerty163|wkj12345678" > user.list
-echo "2878|/Info/T493000657/Front/InsideTwo/InsideTwo.aspx?Id=0CFA5440348BDE13" > car.list.debug
 
 # set crontab
 crontab -l > /tmp/cron.tmp
-cat crontab.dat >> /tmp/cron.tmp
+cat config/crontab.dat >> /tmp/cron.tmp
 crontab /tmp/cron.tmp
 
 # set dependences
-apt-get install samba
-apt-get install smbfs
-apt-get install cifs-utils
-apt-get install tesseract-ocr 
-apt-get install heirloom-mailx
-apt-get install imagemagick
-
-mount.cifs //10.29.101.60/rongche_tigger /usr/local/apps/rongche/tigger -o user=root
-
