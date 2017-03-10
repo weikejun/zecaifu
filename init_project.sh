08e2725f2c681315c0c859650049913a6a415bf9
jim <jim.dev@qq.com>
2017-03-10 15:28:31 +0800

    update

--- init_project.sh
+++ init_project.sh
@@ -4,10 +4,10 @@ cd $(dirname $0)
 source common.sh
 
 # set dirs and files
-mkdir -p log tigger amount http cookies captcha/archives 
+mkdir -p log tigger amount http cookies  
 touch user.list
-chmod 777 log tigger amount http cookies captcha/archives captcha user.list
-echo "qwerty163|wkj12345678" > user.list
+chmod 777 log tigger amount http cookies user.list
+echo "13811311608|Wkj12345678" > user.list
 
 # set crontab
 crontab -l > /tmp/cron.tmp
