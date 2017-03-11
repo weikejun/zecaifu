e83868e6c2ffe38bc10ba99899658883df3c9032
jim <jim.dev@qq.com>
2017-03-11 15:08:05 +0800

    update

--- tools/parse_cars.php
+++ tools/parse_cars.php
@@ -10,6 +10,9 @@ $ret = json_decode($argv[1], true);
 if($ret['data']['borrow']['totalItems'] > 0) {
 	$cars = $ret['data']['borrow']['borrowList'];
 	foreach($cars as $idx => $info) {
+		if($info['status'] != 'run') {
+			continue;
+		}
 		preg_match_all('/第([0-9]+)/', $info['borrowName'], $matches);
 		echo $info['sid'] . '_' . $matches[1][0] . " ";
 	}
