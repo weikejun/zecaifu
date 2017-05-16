99075b000616421c5888532783a87595fe11ec0d
jim <jim.dev@qq.com>
2017-05-16 20:23:22 +0800

    add node list

---
+++ www/list.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+  <title>ZeRobot - 节点列表</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="renderer" content="webkit">
+  <meta http-equiv="Cache-Control" content="no-siteapp" />
+  <link rel="alternate icon" type="image/png" href="/i/favicon.png">
+  <link rel="stylesheet" href="css/amazeui.min.css"/>
+  <style>
+    .header {
+      text-align: center;
+    }
+    .header h1 {
+      font-size: 200%;
+      color: #333;
+      margin-top: 30px;
+    }
+    .header p {
+      font-size: 14px;
+    }
+  </style>
+</head>
+<body>
+<div class="header">
+
+  <hr />
+</div>
+<div class="am-g">
+  <div class="am-u-lg-6 am-u-md-8 am-u-sm-centered">
+    <h3>节点列表</h3>
+    <hr>
+    <div><a href="http://101.200.125.62/index.php">101.200.125.62</a></div>
+    <div><a href="http://182.92.116.75/index.php">182.92.116.75</a></div>
+    <div><a href="http://118.190.23.53/zecaifu/index.php">118.190.23.53</a></div>
+    <hr>
+    <script>var dt=new Date();document.write('<p>© '+dt.getFullYear()+' Jimwei </p>');</script>
+  </div>
+</div>
+</body>
+</html>
+
