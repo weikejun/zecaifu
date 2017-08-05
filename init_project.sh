22f57b266cba2c0abc8b7a3892fca44b8c441482
jim <jim.dev@qq.com>
2017-08-05 13:30:45 +0800

    update

--- init_project.sh
+++ init_project.sh
@@ -6,9 +6,9 @@ source common.sh
 # set dirs and files
 mkdir -p log amount http cookies www/captcha/archives www/captcha/login /tmp/login
 touch user.list
-touch config/strategy.dat
+touch config/strategy.dat config/system.dat
 chmod 777 log amount http cookies www/captcha/archives www/captcha/login /tmp/login
-chmod 666 user.list config/strategy.dat
+chmod 666 user.list config/strategy.dat config/system.dat
 #echo "13811311608|Wkj12345678|bababu523|78250611@qq.com" > user.list
 
 # set crontab
